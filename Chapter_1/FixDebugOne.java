466c7b2d1d6f7eb1c39226bccbd781fac31cfaf1
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-08-29 16:50:00 +0800

    added fix debug one file

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,19 @@
+public class FixDebugOne {
+/* This program displays a greeting */
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. Removed the space between FixDebug and One.
+2. Corrected parameter inside main method.
+3. Corrected spelling in System.out.println.
+4. Added statement terminator (;) in System.out.println.
+5. Added closing tag in main class.
+6. Corrected method name from Main to main
+*/
