28c4103a741ce30ce4aa832c52d90193f2acead4
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-08-29 16:24:05 +0800

    created 3 file

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,25 @@
+/**
+ * Creating A Java Program
+ * Author: Carl Cadungog
+ * Date:8/29/2025
+ * This is about the 3-different types of comment in java.
+ */
+public class Comments {
+
+    public static void main(String[] args) {
+        System.out.println("3-Different Types of Comment Java"); 
+        
+        // Start - variables
+        int a = 150;
+        int b = 3;
+        int product = a * b;
+        // End - variables
+        System.out.println("Product: " + product); // Display the result of multiplying 'a' and 'b'
+
+
+        /*
+         * You can also add multiple line comments here.
+         * It can span multiple lines and is often used to explain more complex code logic.
+         */
+    }
+}
