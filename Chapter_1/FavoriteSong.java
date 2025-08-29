c8a9dec5e9a9464da1fcaa6ab4b27162c834172b
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-08-29 16:26:21 +0800

    changed favorite song lines

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,8 +1,8 @@
 public class FavoriteSong {
     public static void main(String[] args) {
-        System.out.println("Line 1 of my favorite song");
-        System.out.println("Line 2 of my favorite song");
-        System.out.println("Line 3 of my favorite song");
-        System.out.println("Line 4 of my favorite song");
+        System.out.println("You're all I can think of, every drop I drink up");
+        System.out.println("You're my soda pop, my little soda pop");
+        System.out.println("Cool me down, you're so hot, pour me up, I won't stop");
+        System.out.println("You're my soda pop, my little soda pop");
     }
 }
