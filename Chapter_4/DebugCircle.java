481a4859a3c913021ba63ed0e54b0ce184966bc3
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-09-23 19:20:47 +0800

    created file

---
+++ Chapter_4/DebugCircle.java
@@ -0,0 +1,42 @@
+
+  
+   public class DebugCircle
+   {
+   
+   public static void main(String[] args)
+   {
+   
+    }
+
+
+      private int radius;
+      private int diameter;
+      private final double PI = 3.14159;
+      private double area;
+      
+   public DebugCircle(int r)
+   {
+   
+         radius = r;
+         diameter = 2 * r;
+         area = PI * r * r;
+      }
+      
+   public int getRadius()
+   {
+      return radius;
+   }
+   
+   public double getDiameter()
+   {
+      return diameter;
+   }
+   
+   public double getArea()
+   {
+      return area;
+   }
+   
+ 
+}
+
