17c72aa8669269e11c4bbec2d81ef3c15b068dbe
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-09-26 12:06:40 +0800

    changed debugfour3

--- Chapter_4/DebugFour3.java
+++ Chapter_4/DebugFour3.java
@@ -2,32 +2,43 @@
 // This class uses a DebugBox class
 // to instantiate two Box objects
 import java.util.Scanner;
-public class DebugFour3
-{
-public static void main(String args[])
-{
-Scanner input = new Scanner(System.in);
-int w, l, h;
-System.out.print("Enter width of box >> ");
-w = input.nextInteger();
-System.out.print("Enter length of box >> ");
-l = input.nextDouble();
-System.out.print("Enter height of box >> ");
-h = input.nextInt();
-DebugBox box1 = new FixDebugBox();
-DebugBox box1 = new FixDebugBox(w, l, h);
-System.out.println("The dimensions of the first box are");
-box1.showData();
-System.out.print(" The volume of the first box is ");
-showVolume(box1);
-System.out.println("The dimensions of the second box are");
-box2.showData();
-System.out.print(" The volume of the second box is ");
-box2.showVolume();
-}
-public static void showVolume(DebugBox aBox)
-{
-double vol = aBox.getVolume();
-System.out.println(vol);
-}
+
+    public class DebugFour3
+    {
+        public static void main(String [] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+            int w, l, h;
+
+            System.out.print("Enter width of box >> ");
+            w = scanner.nextInt();
+
+            System.out.print("Enter length of box >> ");
+            l = scanner.nextInt();
+
+            System.out.print("Enter height of box >> ");
+            h = scanner.nextInt();
+
+            
+            DebugBox box1 = new DebugBox(w, l, h);
+            DebugBox box2 = new DebugBox(w, l, h);
+          
+
+            System.out.println("The dimensions of the first box are");
+            box1.showData();
+
+            System.out.println(" The volume of the first box is ");
+            showVolume(box1);
+
+            System.out.println("The dimensions of the second box are");
+            box2.showData();
+
+            System.out.println(" The volume of the second box is ");
+            showVolume(box2);
+    }
+        public static void showVolume(DebugBox aBox)
+        {
+            double vol = aBox.getVolume();
+            System.out.println(vol);
+    }
 }
