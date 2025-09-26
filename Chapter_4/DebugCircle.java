59aa9bc960b7aa3c68c12b005b7f43d7082a5f86
CarlAndreiCadungog-UCM <carlcarlcadz@gmail.com>
2025-09-26 11:51:41 +0800

    files

--- Chapter_4/DebugCircle.java
+++ Chapter_4/DebugCircle.java
@@ -1,36 +1,35 @@
-
-  
-public class DebugCircle
-{
-   
-      private int radius;
-      private int diameter;
-      private final double PI = 3.14159;
-      private double area;
-      
-   public DebugCircle(int r)
+ 
+   public class DebugCircle
    {
-   
-         radius = r;
-         diameter = 2 * r;
-         area = PI * r * r;
+      
+         private int radius;
+         private int diameter;
+         private final double PI = 3.14159;
+         private double area;
+         
+         public DebugCircle(int r)
+         {
+         
+               radius = r;
+               diameter = 2 * r;
+               area = PI * r * r;
       }
-      
-   public int getRadius()
-   {
-      return radius;
-   }
-   
-   public double getDiameter()
-   {
-      return diameter;
-   }
-   
-   public double getArea()
-   {
-      return area;
-   }
-   
+            
+         public int getRadius()
+         {
+            return radius;
+      }
+         
+         public double getDiameter()
+         {
+            return diameter;
+      }
+         
+         public double getArea()
+         {
+            return area;
+      }
+         
  
 }
 
---
+++ Chapter_4/DebugFour1.java
@@ -0,0 +1,21 @@
+// This class uses a DebugCircle class
+// to instantiate a Circle object
+import java.util.Scanner;
+
+    public class DebugFour1
+    {
+        public static void main(String [] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+            int radius;
+            
+
+            System.out.print("Enter a radius for a circle >> ");
+            radius = scanner.nextInt();
+            DebugCircle c = new DebugCircle(radius);
+
+            System.out.println("The radius is " + c.getRadius());
+            System.out.println("The diameter is " + c.getDiameter());
+            System.out.println("The area is " + c.getArea());
+    }
+}
